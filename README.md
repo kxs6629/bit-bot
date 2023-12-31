6777a73f19eb2aa924f90fd1aa34322a6166a780
Kenny S <35939549+kxs6629@users.noreply.github.com>
2023-12-31 17:13:25 -0500

    Vote button implementation started

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Simple Discord bot that allows users to rank users by their bits
 
 ## Criteria
-* Users should be able to +1 or -1 someone for their bit
+* Users can start a vote to +1 or -1 someone for their bit
+* After 30 seconds, the vote ends and the results are posted
+* The resulting score is totaled into the user's overall funny score
 * Users can view a leaderboard when a command is given
---
+++ commands/utility/vote.js
@@ -0,0 +1,42 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder} = require('discord.js');
+
+module.exports = {
+    cooldown: 30,
+    data: new SlashCommandBuilder()
+    .setName("vote")
+    .setDescription("Start a vote to rate someone's bit"),
+    async execute(interaction){
+        const target = interaction.options.getUser('target');
+
+        const plus = new ButtonBuilder()
+        .setCustomId('plus')
+        .setLabel('+1')
+        .setStyle(ButtonStyle.Success);
+        // .setEmoji('')
+
+        const minus = new ButtonBuilder()
+        .setCustomId('minus')
+        .setLabel('-1')
+        .setStyle(ButtonStyle.Danger);
+        // .setEmoji('')
+
+        const row = new ActionRowBuilder()
+        .addComponents(plus, minus);
+
+        const response = await interaction.reply({
+            content: `Did ${target} make a funny?`,
+            components: [row],
+        });
+
+        const collector = response.createMessageComponentCollector({componentType: ComponentType.StringSelect, time:30_000});
+
+        collector.on('collect', async i =>{
+            const selection = i.values[0];
+            await i.reply(`${i.user} has selected ${selection}`);
+        });
+    },
+}
+// Where I left off:
+// Figuring out vote command functionality
+// Do I use buttons?
+// How do I ensure all users can only vote once?
